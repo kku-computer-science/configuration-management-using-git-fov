0fe6f57795835282f14dcdfaa8b5f89196ec40d4
jiajia <onarpa.l@kkumail.com>
2025-12-06 11:14:04 +0000

    Update BubbleSort.java

--- Project/BubbleSort.java
+++ Project/BubbleSort.java
@@ -1,18 +1,18 @@
+package Project;
+
 public class BubbleSort {
+
     public void sort(int[] arr) {
         int n = arr.length;
         
-        // วนลูปผ่านสมาชิกใน array
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                // ถ้าตัวปัจจุบันมากกว่าตัวถัดไป ให้สลับที่
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
             }
-            
         }
     }
 }
