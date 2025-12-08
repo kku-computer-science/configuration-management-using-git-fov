1c16c40571108d8d059afa45da423271bf723eba
Chanyanutch <chanyanutch.p@kkumail.com>
2025-12-09 00:19:24 +0700

    Main_chanyanutchver.

--- Project/Main.java
+++ Project/Main.java
@@ -6,36 +6,32 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Integers :"); //ex: 34 2 10 6
+        String input = scanner.nextLine();
+        String[] stringNumbers = input.trim().split("\\s+");
+        int[] numbers = new int[stringNumbers.length];
 
-        // 1. รับค่า Input (Integers)
-        System.out.print("Enter integers: "); // ex. 3 2 1 5 4
-        String inputLine = scanner.nextLine();
-        String[] strArray = inputLine.split(" ");
-        int[] numbers = new int[strArray.length];
-        
-        for (int i = 0; i < strArray.length; i++) {
-            numbers[i] = Integer.parseInt(strArray[i]);
+        for (int i = 0; i < stringNumbers.length; i++) {
+            numbers[i] = Integer.parseInt(stringNumbers[i]);
         }
 
-        // 2. เลือก Algorithm
-        System.out.println("Choose Algorithm:");
+        System.out.println("Choose Algorithm");
         System.out.println("1. Quick Sort");
         System.out.println("2. Bubble Sort");
         System.out.print("Enter choice (1 or 2): ");
         int choice = scanner.nextInt();
 
-        // 3. เรียกใช้งาน Class Sort ตามที่เลือก
-        if (choice == 1) {
-            QuickSort.quickSort(numbers, 0, numbers.length - 1);
+        if(choice ==1){
+            QuickSort.quickSort(numbers,0, numbers.length -1);
         } else if (choice == 2) {
-            BubbleSort bs = new BubbleSort();
-            bs.sort(numbers);
+            BubbleSort bubbleSort = new BubbleSort();
+            bubbleSort.sort(numbers);
         } else {
             System.out.println("Invalid choice.");
+            scanner.close();
             return;
         }
-
-        // 4. แสดงผลลัพธ์
         System.out.println("Result: " + Arrays.toString(numbers));
+        scanner.close();
     }
 }
